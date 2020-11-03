1d6390fa6963ae19018b71537cd89ccb77d94889
kostya <caxnyk2002@gmail.com>
2020-11-03 11:44:42 +0300

    Updates

---
+++ precache-manifest.3d5c6c8a47d37d5cf9fbbf037a54dfcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa58bc19611d9f2e23c66ef8acd10bb",
+    "url": "/star-wars-db/index.html"
+  },
+  {
+    "revision": "92eac8e91b767521babe",
+    "url": "/star-wars-db/static/css/main.227d80ad.chunk.css"
+  },
+  {
+    "revision": "855df5b5179f6fa6eeff",
+    "url": "/star-wars-db/static/js/2.468a1cc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars-db/static/js/2.468a1cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92eac8e91b767521babe",
+    "url": "/star-wars-db/static/js/main.24ce2a83.chunk.js"
+  },
+  {
+    "revision": "630f5d5f507e233bc1ec",
+    "url": "/star-wars-db/static/js/runtime-main.95ecfa1b.js"
+  }
+]);
